1f8101056073eeedb408d1b11b0f5a86def92d30
Yury Tsarev <yury@upbound.io>
2023-01-06 17:23:47 +0100

    Add example module for transition demo
    Signed-off-by: Yury Tsarev <yury@upbound.io>

---
+++ transition/main.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = var.vpcName
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
---
+++ transition/variables.tf
@@ -0,0 +1,4 @@
+variable "vpcName" {
+  description = "VPC name"
+  type        = string
+}
